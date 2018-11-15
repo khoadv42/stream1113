07e86fd2f64f8e91ea404cf4359ebb4c1b36ac61
khoa <kemattbangbang@gmail.com>
2018-11-15 21:25:16 +0700

    code live 2 nguoi

--- src/app.js
+++ src/app.js
@@ -1,21 +1,4 @@
 const openStream = require('./openStream');
-//openStream();
+openStream();
 // webpack ./src/app.js ./public/bundle.js
 // webpack ./src/app.js --o ./public/bundle.js
-const $ = require('jquery');
-const Peer = require('simple-peer');
-const p = new Peer({ initiator: location.hash === '#1', trickle: false })
- 
-p.on('error', function (err) { console.log('error', err) })
- 
-p.on('signal', function (data) {
-  $('#txtMySignal').val(JSON.stringify(data));
-});
-$('#btnConnect').click(()=>{
-    const friendSignal = JSON.parse($('#txtFriendSignal').val());
-    p.signal(friendSignal);
-});
-p.on('connect',()=>{
-    setInterval(()=>p.send(Math.radom()),2000);
-});
-p.on('data',data=>console.log('Nhan du lieu voi',data));
--- src/openStream.js
+++ src/openStream.js
@@ -1,8 +1,26 @@
 const playVideo = require('./playVideo');
+const $ = require('jquery');
+const Peer = require('simple-peer');
 
 function openStream(){
     navigator.mediaDevices.getUserMedia({audio:true,video:true})
-        .then(stream=>playVideo(stream,'localStream'))
+        .then(stream=>{
+            playVideo(stream,'localStream');
+            const p = new Peer({ initiator: location.hash === '#1', trickle: false,stream:stream })
+ 
+            p.on('error', function (err) { console.log('error', err) })
+            
+            p.on('signal', function (data) {
+            $('#txtMySignal').val(JSON.stringify(data));
+            });
+            $('#btnConnect').click(()=>{
+                const friendSignal = JSON.parse($('#txtFriendSignal').val());
+                p.signal(friendSignal);
+
+            });
+
+            p.on('stream',fStream=>playVideo(fStream,'friendStream'));
+        })
         .catch(err=>console.log(err));
 }
 
